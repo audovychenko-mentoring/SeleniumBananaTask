1fad2286ed92dbb341b04c545b6824c917abec5e
audovychenko-mentoring <udovichenkoanastasiia@gmail.com>
2020-06-04 03:24:23 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumBananaTask")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("SeleniumBananaTask")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("652091b1-c1b6-45ec-95b2-003c582d2f7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumBananaTask
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driverFF;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {            
+            driverFF = new FirefoxDriver(@"C:\Program Files\WebDrivers");
+        }
+
+        [TestMethod]
+        public void BananaSongTest()
+        {
+            driverFF.Navigate().GoToUrl("https://www.google.com/");
+        }
+    }
+}
